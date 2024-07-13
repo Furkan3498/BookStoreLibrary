dcedb60ac519b390625ad22bcb9354250e7638cd
Furkan <89530770+Furkan3498@users.noreply.github.com>
2024-07-13 19:10:55 +0300

    Security UserRole

--- src/main/java/com/example/BookStoreLibrary/model/User.java
+++ src/main/java/com/example/BookStoreLibrary/model/User.java
@@ -1,10 +1,26 @@
 package com.example.BookStoreLibrary.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "user_id")
+@Data
 public class User extends BaseEntity{
+
+
+
+    @Column(unique = true)
+    private String usernane;
+
+
+    private String password;
+
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+
+    private String eMail;
 }
---
+++ src/main/java/com/example/BookStoreLibrary/model/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.BookStoreLibrary.model;
+
+public enum UserRole {
+
+    USER , ADMIN
+}
