1bd207c696fc544cae14db6779e13e32ade416e4
Furkan <89530770+Furkan3498@users.noreply.github.com>
2024-07-13 19:57:41 +0300

    JWT TokenGenerator.java

--- src/main/java/com/example/BookStoreLibrary/Exceptions/GenericException.java
+++ src/main/java/com/example/BookStoreLibrary/Exceptions/GenericException.java
@@ -3,13 +3,17 @@ package com.example.BookStoreLibrary.Exceptions;
 import com.example.BookStoreLibrary.dto.ErrorCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import org.apache.http.HttpStatus;
+import org.springframework.http.HttpStatus;
+
 @SuperBuilder
 @NoArgsConstructor
 @Getter
+@Setter
 public class GenericException {
 
     private HttpStatus httpStatus;
+    private String message;
     private ErrorCode errorCode;
 }
--- src/main/java/com/example/BookStoreLibrary/utils/TokenGenerator.java
+++ src/main/java/com/example/BookStoreLibrary/utils/TokenGenerator.java
@@ -1,15 +1,18 @@
 package com.example.BookStoreLibrary.utils;
 
-import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-public class TokenGenearator {
+@Component
+public class TokenGenerator {
 
 
     @Value("${jwt-variables.KEY}")
@@ -30,4 +33,42 @@ public class TokenGenearator {
                 .withExpiresAt( new Date(System.currentTimeMillis()+ (EXPIRES_ACCESS_TOKEN_MINUTES * 60 * 1000)))
                 .sign(Algorithm.HMAC256(KEY.getBytes()));
     }
+
+
+    public DecodedJWT verifyJWT(String token){
+        Algorithm algorithm = Algorithm.HMAC256(KEY.getBytes());
+
+        JWTVerifier jwtVerifier= JWT.require(algorithm).build();
+        return  jwtVerifier.verify(token);
+
+       /* try {
+            return  jwtVerifier.verify(token);
+        }
+        catch (Exception e ){
+            throw GenericException.builder()
+                    .httpStatus(HttpStatus.BAD_REQUEST)
+                    .message(e.getMessage()).build();
+        }
+*/
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
