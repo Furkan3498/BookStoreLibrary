bd230523c09a66400144720f7fb58b267c52d362
Furkan <89530770+Furkan3498@users.noreply.github.com>
2024-07-24 23:49:26 +0300

    JwtAccessDeniedHandler

--- src/main/java/com/example/BookStoreLibrary/configure/SecurityConfig.java
+++ src/main/java/com/example/BookStoreLibrary/configure/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.example.BookStoreLibrary.configure;
 
 
+import com.example.BookStoreLibrary.Security.JwtAccessDeniedHandler;
+import com.example.BookStoreLibrary.Security.JwtAuthenticationEntryPoint;
 import com.example.BookStoreLibrary.Security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,21 +15,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
-private final JwtFilter jwtFilter;
+    private final JwtFilter jwtFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint authenticationEntryPoint;
 
-    public SecurityConfig(JwtFilter jwtFilter) {
+    public SecurityConfig(JwtFilter jwtFilter, JwtAccessDeniedHandler jwtAccessDeniedHandler, JwtAuthenticationEntryPoint authenticationEntryPoint) {
         this.jwtFilter = jwtFilter;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
     }
+
+
+
 
     @Bean
     public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
-        return  configuration.getAuthenticationManager();
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -35,23 +45,26 @@ private final JwtFilter jwtFilter;
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors->cors.configure(http))
+                .cors(cors -> cors.configure(http))
 
                 .authorizeRequests((auth) -> {
-            auth.requestMatchers("/api/admin").hasAuthority("ADMIN");
-            auth.requestMatchers("/api/user").hasAnyAuthority("ADMIN" ,"USER");
-            auth.anyRequest().authenticated();
-        })
+                    auth.requestMatchers("/api/admin").hasAuthority("ADMIN");
+                    auth.requestMatchers("/api/user").hasAnyAuthority("ADMIN", "USER");
+                    auth.anyRequest().authenticated();
+                })
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(httpBasic -> httpBasic.disable())
+                .authenticationManager((AuthenticationManager) authenticationEntryPoint)// look again! there are be wrong
+                .exceptionHandling(exp -> exp.accessDeniedHandler(jwtAccessDeniedHandler))
+
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
 
-
     }
-    public WebSecurityCustomizer webSecurityCustomizer(){
+
+    public WebSecurityCustomizer webSecurityCustomizer() {
         return (web -> web.ignoring().requestMatchers("/api/images/**", "/api/auth/login"));
     }
 }
